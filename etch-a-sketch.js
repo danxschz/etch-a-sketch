1b1eda6a9be6dce8558d54a144e6e2c3436e0845
Daniel Sanchez <danxschz@gmail.com>
2022-04-10 21:20:10 -0400

    Add change grid size feature
    - Refactor code into functions
    - Create change grid size function
    - Implement clear grid function
    - Modify css to work with new feature

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,26 +1,76 @@
 // Create grid
+let currentSize;
 
-let squareDiv;
-let squareRow;
-const gridContainer = document.querySelector('.grid-container');
+function createGrid(size) {
+  let squareDiv;
+  let squareRow;
+  const gridContainer = document.querySelector('.grid');
 
-for (let i = 0; i < 16; i++) {
-  squareRow = document.createElement('div');
-  squareRow.classList.add('square-row');
+  for (let i = 0; i < size; i++) {
+    squareRow = document.createElement('div');
+    squareRow.classList.add('square-row');
 
-  for (let i = 0; i < 16; i++) {
-    squareDiv = document.createElement('div');
-    squareDiv.classList.add('square');
-    squareRow.appendChild(squareDiv);
+    for (let i = 0; i < size; i++) {
+      squareDiv = document.createElement('div');
+      squareDiv.classList.add('square');
+      squareRow.appendChild(squareDiv);
+    }
+    gridContainer.appendChild(squareRow);
   }
-  gridContainer.appendChild(squareRow);
+  currentSize = size;
 }
 
-// Color grid
-const squares = document.querySelectorAll('.square');
+createGrid(48);
 
-squares.forEach(square => {
-  square.addEventListener('mouseover', () => {
-    square.classList.add('colored');
+
+// Delete grid
+function deleteGrid() {
+  const squareRows = document.querySelectorAll('.square-row');
+  squareRows.forEach(row => {
+    row.remove();
   });
+}
+
+
+// Color grid
+function colorGrid() {
+  const squares = document.querySelectorAll('.square');
+
+  squares.forEach(square => {
+    square.addEventListener('mouseover', () => {
+      square.classList.add('colored');
+    });
+  });
+}
+
+colorGrid();
+
+
+// Change grid size
+function changeSize() {
+  size = prompt('Enter new size (10-100)');
+  if ((size < 10) || (size > 100)) {
+    return;
+  }
+  deleteGrid();
+  createGrid(size);
+  colorGrid();
+}
+
+const sizeButton = document.querySelector('.change-size');
+sizeButton.addEventListener('click', () => {
+  changeSize();
+});
+
+
+// Clean grid
+function cleanGrid() {
+  deleteGrid();
+  createGrid(currentSize);
+  colorGrid();
+}
+
+const cleanButton = document.querySelector('.clean-grid');
+cleanButton.addEventListener('click', () => {
+  cleanGrid();
 });
